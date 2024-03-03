7dc0b1b3c55777380b03b897b83a12565284a04b
Minin Dmitriy <116705540+dmitriy-minin@users.noreply.github.com>
2024-03-03 18:51:09 +0500

    Genres
    Fixed view for genres. Now it works.

--- KinoSite/app/Http/Controllers/GenresController.php
+++ KinoSite/app/Http/Controllers/GenresController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Genre;
 
 class GenresController extends Controller
 {
@@ -11,6 +12,7 @@ class GenresController extends Controller
     }
 
     public function genres(){
-        return view('genre.genres');
+        $genres = Genre::all();
+        return view('genre.genres', compact('genres'));
     }
 }
